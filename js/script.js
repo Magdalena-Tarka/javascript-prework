f69b11c9430d0ac95df5b6c38439acc62a90e762
Magdalena Tarka <mxtarka@gmail.com>
2021-01-17 17:44:27 +0100

    Add function to game-game reasults

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Computer moves
+// Computer Moves
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
@@ -16,7 +16,7 @@ if (randomNumber == 1) {
 
 printMessage('Mój ruch to: ' + computerMove);
 
-// Player moves
+// Player Moves
 
 let playerInput = prompt('Wybierz Swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -33,3 +33,15 @@ if (playerInput == 1) {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+// Game Reasults
+
+if (computerMove == playerMove) {
+    printMessage('Remis!');
+} else if ((computerMove == 'kamień' && playerMove == 'papier') || (computerMove == 'papier' && playerMove == 'nożyce') || (computerMove == 'nożyce' && playerMove == 'kamień')) {
+    printMessage('Ty wygrywasz!!');
+} else if ((computerMove == 'kamień' && playerMove == 'nożyce') || (computerMove == 'papier' && playerMove == 'kamień') || (computerMove == 'nożyce' && playerMove == 'papier')) {
+    printMessage('Niestety przegrywasz.. Spróbuj ponownie..');
+} else if (playerMove == 'nieznany ruch') {
+    printMessage('Wybrałeś niepoprawny ruch! Wybierz ponownie!');
+}
